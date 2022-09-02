800449ccd20d7a9867eb29d3d60f600a70f1465f
mlboczar <mlboczar@gmail.com>
2022-09-02 11:43:59 -0400

    After class demo

--- app.js
+++ app.js
@@ -14,6 +14,18 @@ const setup = async () => {
     app.use('/houses', houseRouter);
     app.use('/students', studentRouter);
 
+    //Custom 404 response
+    //there's no error that happened, route couldn't be found
+    app.use((req, res) => {
+      res.status(404).send('No spells here - try again!');
+    });
+
+    //500 & other error handling
+    app.use((err, req, res, next) => {
+      const status = err.status || 500;
+      res.status(status).send(err.message);
+    });
+
     // syncing the database before listening to the port!
     await db.sync();
     app.listen(PORT, () => {
--- db/house.js
+++ db/house.js
@@ -16,4 +16,17 @@ const House = db.define('house', {
   ghost: Sequelize.STRING
 });
 
+//class method
+House.getEverything = async function() {
+  const houses = await House.findAll({
+    include: Student
+  });
+  return houses;
+}
+
+//instance method
+House.prototype.colorStatement = function() {
+  return `The house ${this.name} has the colors ${this.colorPrimary} and ${this.colorSecondary}.`;
+}
+
 module.exports = House;
